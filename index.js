8caf05b8c4241274feac3754e85ceb8d3e1a97a5
Himanshu <codebreaker8055@gmail.com>
2022-03-25 19:36:12 +0330

    First-JavaScript-File

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1. Ways to print in Javascript
+// console.log("Hello world");
+// alert("ME");
+// document.write("This is document write");
+
+// 2. Javascript console API
+// console.log("Hello world", 4 + 6, "Another log");
+// console.warn("This is warning");
+// console.error("This is an Error");
+
+// 3. Javascript variabels
+var number1 = 34;
+var number2 = 56;
+console.log(number1 + number2);
+
+// 4. Data types in Javascript
+
+// Number
+var num1 = 455;
+var num2 = 45.454;
+
+// String
+var str1 = "This is a String";
+var str2 = "This is a String";
+
+// object
+var marks = {
+  ravi: 34,
+  shubham: 78,
+  harry: 99.9,
+};
+console.log(marks);
+
+// Booleans
+var a = true;
+var b = false;
+console.log(a);
