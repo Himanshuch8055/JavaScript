99cd7f581bba2425b22389843966248edf462e6b
Himanshu <codebreaker8055@gmail.com>
2022-03-28 16:39:53 +0330

    Convert-(app.js)-class-component-to-function-component

--- Book-Worm-Udemy-Tutorial/App.js
+++ Book-Worm-Udemy-Tutorial/App.js
@@ -1,126 +1,185 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   SafeAreaView,
   TouchableOpacity,
   TextInput,
+  FlatList,
 } from "react-native";
 import BookCount from "./src/Components/BookCount";
 import { Ionicons } from "@expo/vector-icons";
 
-// const App = () => {
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      totalCount: 0,
-      readingCount: 0,
-      readCount: 0,
-      isAddNewBookVisible: false,
-      textInputData: true,
-    };
-  }
-  showAddNewBook = () => {
-    this.setState({ isAddNewBookVisible: true });
+const App = () => {
+  const [totalCount, setTotalCount] = useState(10);
+  const [readCount, setReadCount] = useState(0);
+  const [readingCount, setReadingCount] = useState(0);
+  const [isAddNewBookVisible, setIsAddNewBookVisible] = useState(false);
+  const [textInputData, setTextInputData] = useState("");
+  const [books, setBooks] = useState([]);
+  const [bookData, setBookData] = useState({
+    author: "",
+    publisher: "",
+  });
+
+  const showAddNewBook = () => {
+    setIsAddNewBookVisible(true);
   };
-  hideAddNewBook = () => {
-    this.setState({ hideAddNewBook: false });
+
+  const hideAddNewBook = () => {
+    setIsAddNewBookVisible(() => {
+      return false;
+    });
   };
-  addBook = () => {
-    alert(this.state, textInputData);
+
+  const addBook = (book) => {
+    setBooks((prev) => {
+      return [...prev, book];
+    });
+
+    setTotalCount((pre) => {
+      return pre + 1;
+    });
+
+    setReadCount((pre) => {
+      return pre + 1;
+    });
+
+    setBookData((pre) => {
+      return { ...pre, author: "Varun Nath" };
+    });
   };
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <SafeAreaView style={{ height: 40 }} />
+
+  const markAsRead = (selectedBook, index) => {
+    let newList = books.filter((book) => book !== selectedBook);
+  };
+
+  const renderItem = (item, index) => (
+    <View style={{ height: 50, flexDirection: "row" }}>
+      <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+        <Text>{item}</Text>
+      </View>
+      <TouchableOpacity onPress={() => markAsRead(item, index)}>
         <View
           style={{
-            height: 70,
-            borderBottomWidth: 0.5,
-            borderBottomColor: "#000000",
+            width: 100,
+            height: 50,
+            backgroundColor: "#90ee90",
             alignItems: "center",
             justifyContent: "center",
           }}
         >
-          <Text style={{ fontSize: 24 }}>Book Worm</Text>
+          <Text style={{ fontWeight: "bold", color: "#ffffff" }}>
+            Mark as Read
+          </Text>
         </View>
-        <View style={{ flex: 1 }}>
-          {this.state.isAddNewBookVisible && (
-            <View style={{ height: 50, flexDirection: "row" }}>
-              <TextInput
-                onChangeText={(text) => this.setState({ textInputData: text })}
-                style={{
-                  flex: 1,
-                  backgroundColor: "#d3d3d3",
-                  padding: 10,
-                  fontStyle: "italic",
-                }}
-                placeholder="Enter Book Name"
-                placeholderTextColor="#a9a9a9"
-              />
-              <TouchableOpacity onPress={this.addBook}>
-                <View
-                  style={{
-                    width: 50,
-                    height: 50,
-                    backgroundColor: "#90ee90",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <Ionicons name="ios-checkmark" color="#f8f8ff" size={40} />
-                </View>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={this.hideAddNewBook}>
-                <View
-                  style={{
-                    width: 50,
-                    height: 50,
-                    backgroundColor: "#ff8080",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <Ionicons name="ios-close" color="#f8f8ff" size={40} />
-                </View>
-              </TouchableOpacity>
-            </View>
-          )}
-          <TouchableOpacity
-            onPress={this.showAddNewBook}
-            style={{ position: "absolute", bottom: 20, right: 20 }}
-          >
-            <View
-              style={{
-                width: 50,
-                height: 50,
-                borderRadius: 25,
-                backgroundColor: "#4dd2ff",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              <Text style={{ color: "#000000", fontSize: 30 }}>+</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            height: 70,
-            borderTopWidth: 0.5,
-            borderTopColor: "#000000",
-            flexDirection: "row",
-          }}
-        >
-          <BookCount title="Total" count={this.state.totalCount} />
-          <BookCount title="Reading" count={this.state.readingCount} />
-          <BookCount title="Read" count={this.state.readCount} />
-        </View>
-        <SafeAreaView style={{ height: 40 }} />
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <View style={{ flex: 1 }}>
+      <SafeAreaView style={{ height: 40, backgroundColor: "#ffe6b3" }} />
+      <View
+        style={{
+          height: 70,
+          borderBottomWidth: 0.5,
+          borderBottomColor: "#000000",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#ffe6b3",
+        }}
+      >
+        <Text style={{ fontSize: 24 }}>Book Worm</Text>
       </View>
-    );
-  }
-}
+      <View style={{ flex: 1 }}>
+        {isAddNewBookVisible && (
+          <View style={{ height: 50, flexDirection: "row" }}>
+            <TextInput
+              onChangeText={(text) => setTextInputData(text)}
+              style={{
+                flex: 1,
+                backgroundColor: "#d3d3d3",
+                padding: 10,
+                // fontStyle: "italic",
+              }}
+              placeholder="Enter Book Name"
+              placeholderTextColor="#ffffff"
+            />
+            <TouchableOpacity onPress={() => addBook(textInputData)}>
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  backgroundColor: "#90ee90",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="ios-checkmark" color="#ffffff" size={40} />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={hideAddNewBook}>
+              <View
+                style={{
+                  width: 50,
+                  height: 50,
+                  backgroundColor: "#ff8080",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Ionicons name="ios-close" color="#ffffff" size={40} />
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        <FlatList
+          data={books}
+          renderItem={({ item }, index) => renderItem(item, index)}
+          keyExtractor={(item, index) => index.toString()}
+          ListEmptyComponent={
+            <View style={{ marginTop: 50, alignItems: "center" }}>
+              <Text style={{ fontWeight: "bold" }}>Not Reading Any Books.</Text>
+            </View>
+          }
+        />
+
+        <TouchableOpacity
+          onPress={showAddNewBook}
+          style={{ position: "absolute", bottom: 20, right: 20 }}
+        >
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              borderRadius: 25,
+              backgroundColor: "#4dd2ff",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ color: "#ffffff", fontSize: 30 }}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          height: 70,
+          borderTopWidth: 0.5,
+          borderTopColor: "#000000",
+          flexDirection: "row",
+          backgroundColor: "#ffeecc",
+        }}
+      >
+        <BookCount title="Total" count={totalCount} />
+        <BookCount title="Reading" count={readingCount} />
+        <BookCount title="Read" count={readCount} />
+      </View>
+      <SafeAreaView style={{ height: 40, backgroundColor: "#ffeecc" }} />
+    </View>
+  );
+};
 
 export default App;
